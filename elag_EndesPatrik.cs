c11b5831982d86868d98ee46b5c656b43dbb7c5b
endes12 <35299563+endes12@users.noreply.github.com>
2019-01-24 14:47:38 +0100

    Add files via upload

---
+++ elag_EndesPatrik.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elag_EndesPatrik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szia! \n\tKérlek írd be a neved!");
+            string nev = Console.ReadLine();
+            Console.WriteLine(nev);
+            Console.WriteLine("Kérlek írd be a nemed!\n\t");
+            string Nem = Console.ReadLine();
+            Console.WriteLine(Nem);
+            Console.WriteLine("Üdv a {0} táborban {1}\t",Nem,nev);
+            if (Nem=="férfi")
+            {
+                Console.WriteLine("Legénybúcsúba várlak");
+            }
+            if(Nem=="Nő" ||Nem== "csaj") 
+            {
+            
+                Console.WriteLine("Babazsúr");
+            }
+            if (Nem!="férfi"&& Nem!="nő"&& Nem!="csaj")
+            {
+
+
+                Console.WriteLine("Maradj otthon");
+            }
+            
+        
+        
+        
+        
+            
+            
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
