a8d05e9686e982d0096f88b74b0d56151803cd67
endes12 <35299563+endes12@users.noreply.github.com>
2019-01-31 14:56:53 +0100

    Add files via upload

---
+++ program_I_31.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            
+            Console.Write("Szia \n Kérlek írd be a neved?");
+            string nev = Console.ReadLine();
+            Console.WriteLine("Írj be egy jelszót");
+            string jel = Console.ReadLine();
+            if (nev == "Patrik" || nev == "Andi")
+            {
+                Console.WriteLine("Jó a név");
+                if (nev!="Patrik" || nev!="Gyuri")
+                {
+                    Console.WriteLine("Rossz a név nem mehetsz tovább");
+                    if (jel=="1" || jel=="2")
+                    {
+                        Console.Clear();
+                    }
+                   
+                    
+                }
+            
+            
+            }
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
